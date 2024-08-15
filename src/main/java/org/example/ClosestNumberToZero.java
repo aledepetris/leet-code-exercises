92f08ec2e21c352258d80a70403375ce93e7da2e
alejandro.depetris <alejandro.depetris@neoris.com>
2024-08-15 15:49:17 -0300

    First LeetCode excersice - ClosestNumberToZero

---
+++ src/main/java/org/example/ClosestNumberToZero.java
@@ -0,0 +1,25 @@
+package org.example;
+
+/**
+ * 1768 - Given an integer array nums of size n, return the number with the value closest to 0 in nums.
+ * If there are multiple answers, return the number with the largest value.
+ */
+public class ClosestNumberToZero {
+
+    public int findClosestNumber(int[] nums) {
+        int minDistance = Math.abs(nums[0]);
+        int closest = nums[0];
+
+        for (int i =1; i < nums.length; i++) {
+            int distanceToZero = Math.abs(nums[i]);
+            if (distanceToZero < minDistance) {
+                minDistance = distanceToZero;
+                closest = nums[i];
+            } else if (distanceToZero == minDistance && closest < nums[i]) {
+                closest = nums[i];
+            }
+
+        }
+        return closest;
+    }
+}
---
+++ src/test/java/TestFindClosestNumber.java
@@ -0,0 +1,32 @@
+import org.example.ClosestNumberToZero;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class TestFindClosestNumber {
+
+    private ClosestNumberToZero closestNumberToZero;
+
+    @BeforeEach
+    public void setUp() {
+        closestNumberToZero = new ClosestNumberToZero();
+    }
+
+    @Test
+    void findClosestNumberToZero() {
+
+        int [] numbers = {-5555,-30,-1,2,5,7,1523};
+        int result = closestNumberToZero.findClosestNumber(numbers);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void findClosestNumberToWithEqualDistance() {
+
+        int [] numbers = {1,-1,2,5,7};
+        int result = closestNumberToZero.findClosestNumber(numbers);
+        assertThat(result).isEqualTo(1);
+    }
+
+}
