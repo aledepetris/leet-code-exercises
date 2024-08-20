fce3b16db360d632a57deab6f1b9c5f7fc2ac848
alejandro.depetris <alejandro.depetris@neoris.com>
2024-08-20 12:16:20 -0300

    LeetCode Exercise #5 - N°121. Best Time to Buy and Sell Stock

---
+++ src/main/java/org/example/SellAndBuyStock.java
@@ -0,0 +1,32 @@
+package org.example;
+
+/**
+ * 121. Best Time to Buy and Sell Stock
+ * You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day
+ * in the future to sell that stock.
+ * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ */
+public class SellAndBuyStock {
+
+    public int maxProfit(int[] prices) {
+
+        int size = prices.length;
+        int maxProfit = 0;
+        int min = prices[0];
+
+        for (int i = 1; i < size; i++) {
+            if(prices[i] > min) {
+                int profit = prices[i] - min;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            } else {
+                min = prices[i];
+            }
+        }
+
+        return maxProfit;
+    }
+
+}
---
+++ src/test/java/TestSellAndBuyStock.java
@@ -0,0 +1,33 @@
+import org.example.SellAndBuyStock;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class TestSellAndBuyStock {
+
+    SellAndBuyStock stockTrade = new SellAndBuyStock();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void testRomanToIntExample(int[] numbers, int resulExpected) {
+        assertThat(stockTrade.maxProfit(numbers)).isEqualTo(resulExpected);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new int[]{7, 1, 5, 3, 6, 4}, 5),
+                Arguments.of(new int[]{7, 6, 4, 3, 1}, 0),
+                Arguments.of(new int[]{1, 2, 3, 4, 5}, 4),
+                Arguments.of(new int[]{3, 3, 3, 3, 3}, 0),
+                Arguments.of(new int[]{5}, 0),
+                Arguments.of(new int[]{2, 4, 1}, 2),
+                Arguments.of(new int[]{3, 2, 6, 5, 0, 3}, 4),
+                Arguments.of(new int[]{3, 8, 2, 6, 5, 0, 3}, 5)
+        );
+    }
+
+}
