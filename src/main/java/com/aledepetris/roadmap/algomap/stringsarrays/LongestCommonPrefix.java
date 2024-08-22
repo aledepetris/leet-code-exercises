14fd54edf660f8981a01462e7d800356e49d1226
aledepetris <aledepetris@outlook.com>
2024-08-22 13:44:14 -0300

    LeetCode Exercise #6 - N°14. Longest Common Prefix

---
+++ src/main/java/com/aledepetris/roadmap/algomap/stringsarrays/LongestCommonPrefix.java
@@ -0,0 +1,39 @@
+package com.aledepetris.roadmap.algomap.stringsarrays;
+
+/**
+ * 14. Longest Common Prefix - Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+
+        int minLenght = 200;
+
+        for (String s : strs) {
+            if (s.length() < minLenght) {
+                minLenght = s.length();
+            }
+        }
+
+        boolean match = false;
+        int largest = 0;
+        for (int i = 0; i < minLenght; i++) {
+            char letterToCompare = strs[0].charAt(i);
+            for(String s: strs) {
+                if (letterToCompare == s.charAt(i)) {
+                    match = true;
+                } else {
+                    match = false;
+                    break;
+                }
+            }
+            if (!match) break;
+            largest += 1;
+        }
+
+        return strs[0].substring(0, largest);
+
+    }
+
+}
---
+++ src/test/java/TestLongestCommonPrefix.java
@@ -0,0 +1,36 @@
+import com.aledepetris.roadmap.algomap.stringsarrays.LongestCommonPrefix;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class TestLongestCommonPrefix {
+
+    LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void testRomanToIntExample(String[] strsArray, String expectedPrefix) {
+        assertThat(longestCommonPrefix.longestCommonPrefix(strsArray)).isEqualTo(expectedPrefix);
+    }
+
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of(new String[]{"flower", "flow", "flight"}, "fl"),
+                Arguments.of(new String[]{"dog", "racecar", "car"}, ""),
+                Arguments.of(new String[]{"interspecies", "interstellar", "interstate"}, "inters"),
+                Arguments.of(new String[]{"throne", "dungeon"}, ""),
+                Arguments.of(new String[]{"throne", "throne"}, "throne"),
+                Arguments.of(new String[]{"throne", "thron", "throne"}, "thron"),
+                Arguments.of(new String[]{"", "nonempty"}, ""),
+                Arguments.of(new String[]{"abc", "abcd", "ab"}, "ab"),
+                Arguments.of(new String[]{"a", "a", "a"}, "a"),
+                Arguments.of(new String[]{"longest", "longer", "long"}, "long")
+        );
+    }
+
+}
