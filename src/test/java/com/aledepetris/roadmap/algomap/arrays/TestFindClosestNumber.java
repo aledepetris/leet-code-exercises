4d9ff63531c6193dec3055059edc94b15ea5e2b8
alejandro-depetris <aledepetris@outlook.com>
2025-05-24 12:51:29 -0300

    reorganize project structure

---
+++ src/test/java/com/aledepetris/roadmap/algomap/arrays/TestFindClosestNumber.java
@@ -0,0 +1,34 @@
+package com.aledepetris.roadmap.algomap.arrays;
+
+import com.aledepetris.roadmap.algomap.arrays.ClosestNumberToZero;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class TestFindClosestNumber {
+
+    private ClosestNumberToZero closestNumberToZero;
+
+    @BeforeEach
+    public void setUp() {
+        closestNumberToZero = new ClosestNumberToZero();
+    }
+
+    @Test
+    void findClosestNumberToZero() {
+
+        int [] numbers = {-5555,-30,-1,2,5,7,1523};
+        int result = closestNumberToZero.findClosestNumber(numbers);
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    void findClosestNumberToWithEqualDistance() {
+
+        int [] numbers = {1,-1,2,5,7};
+        int result = closestNumberToZero.findClosestNumber(numbers);
+        assertThat(result).isEqualTo(1);
+    }
+
+}
---
+++ src/test/java/com/aledepetris/roadmap/algomap/arrays/TestMergeStringAlternately.java
@@ -0,0 +1,35 @@
+package com.aledepetris.roadmap.algomap.arrays;
+
+import com.aledepetris.roadmap.algomap.arrays.MergeStringAlternately;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class TestMergeStringAlternately {
+
+    private final MergeStringAlternately merger = new MergeStringAlternately();
+
+    @ParameterizedTest
+    @MethodSource("testData")
+    void testMergeAlternately(String word1, String word2, String expected) {
+        String result = merger.mergeAlternately(word1, word2);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> testData() {
+        return Stream.of(
+                Arguments.of("abc", "def", "adbecf"),
+                Arguments.of("abcd", "xy", "axbycd"),
+                Arguments.of("ab", "xyz", "axbyz"),
+                Arguments.of("", "xyz", "xyz"),
+                Arguments.of("abc", "", "abc"),
+                Arguments.of("", "", "")
+        );
+    }
+
+
+}
