611323a0032c308c9cd28c271b742b4eb433a4e1
aledepetris <aledepetris@outlook.com>
2025-06-11 15:51:15 -0300

    change name of test class

--- src/test/java/com/aledepetris/leetcode/daily/challenge/TestLexicographicalNumbers.java
+++ src/test/java/com/aledepetris/leetcode/daily/challenge/TestLexicographicalNumbers.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.params.provider.Arguments;
 
-class TestLexicalOrder {
+class TestLexicographicalNumbers {
 
     LexicographicalNumbers solution = new LexicographicalNumbers();
 
