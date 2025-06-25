f6617aebd6d8797d8c4fdc46a093ddfdddeae00b
aledepetris <aledepetris@outlook.com>
2025-06-25 12:50:48 -0300

    LeetCode Exercise - Daily Challenge 25-06-25 - Kth Smallest Product of Two Sorted Arrays - Solution with Binary Search

--- src/main/java/com/aledepetris/leetcode/daily/challenge/KthSmallestProductTwoSortedArrays.java
+++ src/main/java/com/aledepetris/leetcode/daily/challenge/KthSmallestProductTwoSortedArrays.java
@@ -4,22 +4,44 @@ import java.util.Arrays;
 
 public class KthSmallestProductTwoSortedArrays {
     public long kthSmallestProduct(int[] nums1, int[] nums2, long k) {
+        long left = -10000000000L;
+        long right = 10000000000L;
 
-        int n = nums1.length;
-        int m = nums2.length;
-
-        long[] products = new long[n*m];
-
-        int idx = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                products[idx] = nums1[i] * nums2[j];
-                idx++;
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+            if (countProducts(nums1, nums2, mid) < k) {
+                left = mid + 1;
+            } else {
+                right = mid;
             }
         }
 
-        Arrays.sort(products);
-        return products[(int) k - 1];
+        return left;
+    }
 
+    private long countProducts(int[] nums1, int[] nums2, long target) {
+        long count = 0;
+        for (int num1 : nums1) {
+            if (num1 == 0) {
+                if (target >= 0) count += nums2.length;
+                continue;
+            }
+
+            int low = 0, high = nums2.length;
+            while (low < high) {
+                int mid = low + (high - low) / 2;
+                long product = (long) num1 * nums2[mid];
+                if (product <= target) {
+                    if (num1 > 0) low = mid + 1;
+                    else high = mid;
+                } else {
+                    if (num1 > 0) high = mid;
+                    else low = mid + 1;
+                }
+            }
+
+            count += (num1 > 0) ? low : nums2.length - low;
+        }
+        return count;
     }
 }
